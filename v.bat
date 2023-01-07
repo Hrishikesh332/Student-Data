2fba6bdabe6ec54c32b24dfb1b7169daa76aa2c0
Hrishikesh <hrishikesh3321@gmail.com>
2023-01-08 02:27:22 +0530

    Structure added

---
+++ v.bat
@@ -0,0 +1,9 @@
+#Notebook
+
+install ```pip install notebook```
+Run ```jupyter notebook```
+
+#Lab
+
+Install ```pip install jupyterlab```
+Run ```jupyter-lab```
---
+++ ven.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Users\Hrishi\Desktop\jupyter\venv\Scripts"
+activate
+
+cd "C:\dir"
